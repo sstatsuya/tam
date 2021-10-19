45411ef8fc949faaa402a2b7db47349d9663891e
sstatsuya <70736475+sstatsuya@users.noreply.github.com>
2021-10-19 22:54:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var musicPlayer = {
             name: 'The fading story',
             singer: 'Mihoyo',
             image: './Assets/music/poster1.png',
-            src: '/Assets/music/song1.mp3',
+            src: '/Assets/music/song10.mp3',
             id: 0,
         },
         {
@@ -46,7 +46,7 @@ var musicPlayer = {
             name: 'The fading story',
             singer: 'Mihoyo',
             image: './Assets/music/poster1.png',
-            src: './Assets/music/song1.mp3',
+            src: './Assets/music/song10.mp3',
             id: 5,
         },
         {
